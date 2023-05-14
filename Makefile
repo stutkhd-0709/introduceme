125d1eafd2d9f0f3218496b5dd518eaff4d7397b
Takahide Sato <sato_t@cyberowl.co.jp>
2023-05-14 20:51:26 +0900

    add tasks api

--- Makefile
+++ Makefile
@@ -2,7 +2,16 @@ build:
 	docker-compose build
 
 up:
-	make build && docker-compose up
+	make build && docker-compose up -d
+
+down:
+	docker-compose down
+
+log-client:
+	docker-compose logs -f client
+
+log-server:
+	docker-compose logs -f server
 
 run-root-bash:
 	docker run --rm -it --name root-todo -w /app -v ${PWD}:/app node:18 bash
--- packages/backend/server.ts
+++ packages/backend/server.ts
@@ -1,6 +1,43 @@
-const express = require('express')
+import express from 'express'
 
 const app = express()
+
+// task
+app.get('/api/tasks', async (req: express.Request, res: express.Response) => {
+  res.status(200).json([
+    {
+      name: 'task1'
+    },
+    {
+      name: 'task2'
+    },
+    {
+      name: 'task3'
+    }
+  ])
+})
+
+app.get('/api/tasks/:id', async (req: express.Request, res: express.Response) => {
+  res.status(200).json({
+    name: 'task1'
+  })
+})
+
+app.post('/api/tasks', async (req: express.Request, res: express.Response) => {
+  res.status(200)
+})
+
+app.put('/api/tasks/:id', async (req: express.Request, res: express.Response) => {
+  res.status(200).json({
+    'name': req.body.name
+  })
+})
+
+app.delete('/api/tasks/:id', async (req: express.Request, res: express.Response) => {
+  res.status(200)
+})
+
+
 app.listen(4000, () => {
   console.log('server running on port 4000')
 })
