525c54fb0c190cec35db96f07c6cf4a9e8cc7012
Takahide Sato <sato_t@cyberowl.co.jp>
2023-05-19 03:13:37 +0900

    fix navigation transition

--- packages/frontend/src/components/HeaderNavigation.vue
+++ packages/frontend/src/components/HeaderNavigation.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="l-header-navigation">
     <template v-for="(navi, idx) in navigations" :key="idx">
-      <RouterLink :to="navi">{{ navi }}</RouterLink>
+      <template v-if="navi.route === '/'">
+        <RouterLink :to="navi.route" class="nav-item" exact>{{ navi.text }}</RouterLink>
+      </template>
+      <template v-else>
+        <RouterLink :to="navi.route" class="nav-item">{{ navi.text }}</RouterLink>
+      </template>
     </template>
   </div>
 </template>
@@ -11,9 +16,9 @@ import { ref } from 'vue'
 import { RouterLink } from 'vue-router'
 
 const navigations = ref([
-  'Top',
-  'Ranking',
-  'Explore'
+  {text: 'Top', route: '/'},
+  {text: 'Ranking', route: 'ranking'},
+  {text: 'Explore', route: 'explore'}
 ])
 </script>
 
@@ -25,5 +30,24 @@ const navigations = ref([
   gap: 0 56px;
   max-width: 1069px;
   padding: 0 16px;
+}
+
+a.nav-item {
+  color: #9A9696;
+  font-size: 24px;
+  font-weight: bold;
+  text-decoration: none;
+  transition: color 0.25s; /* 色の変更だけ適用。hoverもcolorがblackになるので適用される */
+  padding: 12px 0;
+}
+
+a.router-link-active {
+  color: black;
+  border-bottom: 3px solid black;
+  text-decoration: none;
+}
+
+a.nav-item:hover {
+  color: black;
 }
 </style>
