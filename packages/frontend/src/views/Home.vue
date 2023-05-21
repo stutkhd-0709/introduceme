45c7a9504c7dc301d6db3219e3b1390f41c42bbc
Takahide Sato <sato_t@cyberowl.co.jp>
2023-05-21 22:52:51 +0900

    add more load button

--- packages/frontend/src/views/Home.vue
+++ packages/frontend/src/views/Home.vue
@@ -2,29 +2,38 @@
   <div class="l-home-page">
     <div class="f-container">
       <UserCardList/>
+      <a href="users" class="load-more-link">もっと見る<span>→</span></a>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import UserCardList from '@/components/user_card/UserCardList.vue';
-
 </script>
 
 <style>
 .l-home-page {
   background-color: #B6E4E4;
+  padding-bottom: 56px;
 }
 
 .f-container {
   max-width: 740px;
   margin: 0 auto;
-  padding-top: 164px
+  padding-top: 164px;
+  text-align: center;
 }
 
 .card-list-container {
   display: flex;
   flex-wrap: wrap;
   gap: 112px 68px;
+}
+
+.load-more-link {
+  margin: auto;
+  margin-top: 56px;
+  text-decoration: none;
+  display: block
 }
 </style>
